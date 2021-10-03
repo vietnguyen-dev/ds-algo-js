380f78c107da7b473f5cec1a4d281f156b353e56
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2021-10-03 16:00:17 -0700

    starting array section
    shallow copy

---
+++ arrays.js
@@ -0,0 +1,16 @@
+//shallow copy
+//since new array is defined by old array, changes in old will occur in new
+// let arr = []
+// for (let i =0; i <= 3; i++){
+//     arr[i] = i+ 1
+// }
+// let newArr = arr;
+// arr[0] = 'changed';
+// console.log(newArr[0])
+
+//excersize 1. 
+// function person()
+
+// const getGrade = () =>{
+
+// }
--- index.js
+++
@@ -1,1 +0,0 @@
-console.log('hi you stupid prick')
